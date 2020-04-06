5bc13649cfb71a6cf963dd72561dfe7e38e6f3d1
loicndjoyi <loic_ndjoyi@outlook.com>
2020-04-05 21:53:04 -0400

    More items

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "home#index"
   get "/about" => "home#about"
+  post "/questions" => "home#temp"
+  get "/questions/:id" => "home#question"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
